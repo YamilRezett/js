d953389b11cfe129d4aa07ed8562162847feff4d
Yamil Rezett <yamilrezett@gmail.com>
2022-06-01 22:47:24 -0300

    JS

---
+++ app.js
@@ -0,0 +1,17 @@
+let nombreUsuario = prompt("Ingresar nombre de usuario");
+
+if((nombreUsuario == "YAMIL") || (nombreUsuario == "yamil")){
+    alert("El nombre ingresaste es Yamil");
+    }
+else {
+    alert("El nombre ingresado es incorecto");
+}
+
+let contraseña = prompt("Ingresa contraseña");
+
+if(contraseña == " ") {
+    alert("Contraseña Correcta");
+}
+else {
+    alert("Contraseña Incorrecta");
+}
