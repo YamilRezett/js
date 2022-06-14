a65d66ae46b6cf8a900bf736f1d383cb8c328624
Yamil Rezett <yamilrezett@gmail.com>
2022-06-14 19:58:26 -0300

    Desafio Arrays

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let nombreUsuario = prompt("Ingresar nombre de usuario");
+//Ingreso con alerta 
+
+/* let nombreUsuario = prompt("Ingresar nombre de usuario");
 
 if((nombreUsuario == "YAMIL") || (nombreUsuario == "yamil")){
     alert("El nombre ingresaste es Yamil");
@@ -14,9 +16,11 @@ if(contraseña == "js") {
 }
 else {
     alert("Contraseña Incorrecta");
-}
+} */
 
-let operacion = Number(prompt("Que operacion desea realizar? \n1 Suma \n2 Resta \n3 Multiplicación \n4 División \n5 Salir"))
+//Calculadora 
+
+/* let operacion = Number(prompt("Que operacion desea realizar? \n1 Suma \n2 Resta \n3 Multiplicación \n4 División \n5 Salir"))
 
 while (operacion != 5) {
     let numero_Uno = parseFloat(prompt("Ingrese su primer numero"));
@@ -37,9 +41,11 @@ while (operacion != 5) {
     }
     operacion = Number(prompt("Que operacion desea realizar? \n1 Suma \n2 Resta \n3 Multiplicación \n4 División \n5 Salir"))
 
-}
+} */
 
-const suma = (a,b) => a + b
+//Precio de producto 
+
+/* const suma = (a,b) => a + b
 const resta = (a,b) => a - b
 
 const iva = valor => valor * 0.21
@@ -47,4 +53,21 @@ let precioProducto = 500
 let descuento = 50
 
 let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento)
-console.log(nuevoPrecio)
+console.log(nuevoPrecio) */
+
+//DESAFIO CON ARRAYS
+
+const ProdutosEnStock = ["BOTINES", "BOLSO", "PELOA", "CAMISETA", "CAMPERA"];
+let eliminados = [];
+
+const eliminar = (producto) => {
+    let index = ProdutosEnStock.indexOf(producto)
+
+    if (index != -1 ) {
+        eliminados.push(ProdutosEnStock.splice(index, 1));
+    }
+}
+eliminar("BOTINES");
+console.log(ProdutosEnStock.length);
+console.log(eliminados);
+
